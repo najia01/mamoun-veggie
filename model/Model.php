6995f3de3cae60a8ac5bc0084b3cb8eefd137b3b
najia01 <validress@outlook.fr>
2023-04-21 11:48:50 +0200

    change class

---
+++ model/Model.php
@@ -0,0 +1,23 @@
+<?php
+abstract class Model
+{
+    
+    private static $db;
+   
+    private static function setDb()
+   
+    {
+        try {
+            self::$db = new PDO('mysql:host=localhost;dbname=mamoun_veggie;charset=utf8', 'root');
+        } catch (PDOException $e) {
+            echo 'Erreur :' . $e->getMessage();
+        }
+    }
+    protected function getDb()
+    {
+        if (self::$db == null) {
+            self::setDb();
+        }
+        return self::$db;
+    }
+}
