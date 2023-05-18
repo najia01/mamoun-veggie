c311ff08407b217e2505f557f77aa3095c69da59
najia01 <validress@outlook.fr>
2023-05-18 15:08:14 +0200

    formulaire connexion ok front

--- controller/UserController.php
+++ controller/UserController.php
@@ -4,19 +4,31 @@ class UserController extends Controller
 {
     public function userLogin()
     {
+        $model = new UserModel();
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $username = $_POST['username'];
             $password = $_POST['password'];
-            $model = new UserModel();
-         $datas = $model->getUser($username);
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            $mail = filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL);
+
+            $userData = new User([
+                'username' => $username,
+                'password' => $password,
+                'mail' => $mail
+            ]);
+
+            $user = ($userData);
+            $result = $model->getUser($user);
+            global $router;
+            header('Location:' . $router->generate('home'));
+            exit();
+
+        } else {
+
+            global $router;
+            $link = $router->generate('baseUser');
+            echo self::getTwig()->render('connect.html.twig', ['link' => $link]);
         }
-
-
-        global $router;
-
-       
-
-        $link = $router->generate('baseUser');
-        echo self::getTwig()->render('connect.html.twig', ['link' => $link]);
     }
 }
--- model/UserModel.php
+++ model/UserModel.php
@@ -6,7 +6,7 @@ class UserModel extends Model
     public function getUser($username)
     {
         $req = $this->getDb()->prepare('SELECT  `username`,`password` FROM `user`  WHERE `username` = :username');
-        $req->bindParam(':username', $username, PDO::PARAM_STR);
+        $req->bindParam('username', $username, PDO::PARAM_STR);
         $user = $req->fetch(PDO::FETCH_ASSOC);
         $req->closeCursor();
 
